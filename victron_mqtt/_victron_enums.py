170d98d861a376459d50479e171fec00728775a7
SlapJackNpNp <beherzog@web.de>
2025-07-21 15:32:22 +0200

    Update _victron_enums.py
    Updated Naming

--- victron_mqtt/_victron_enums.py
+++ victron_mqtt/_victron_enums.py
@@ -89,14 +89,14 @@ class FluidType(VictronEnum):
     HydraulicOil = (10, "Hydraulic oil")
     RawWater = (11, "Raw water")
     
-class Solarchargerstate(VictronEnum):
+class SolarChargerState(VictronEnum):
     Off = (0, "Off")
-    Voltage/Current_limited = (1, "Voltage/current limited)
-    MPPTactive = (2, "MPPT active")
-    Notavailable = (255, "Not available")
+    VoltageCurrentLimited = (1, "Voltage/current limited)
+    MPPTActive = (2, "MPPT active")
+    NotavAilable = (255, "Not available")
 
 class ESSMode(VictronEnum):
-    selfconsumptionbatterylife = (0, "self consumption (batterylife))
-    selfconsumption = (1, "self consumption)
-    keepcharger = (2, "keep charged")
-    externalcontrol = (3, "External control)
+    SelfConsumptionBatterylife = (0, "self consumption (batterylife))
+    SelfConsumption = (1, "self consumption)
+    KeepCharged = (2, "keep charged")
+    ExternalControl = (3, "External control)
