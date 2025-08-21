92707bcdffdfbb9397a4c2e1e953ffee46689af6
Tomer <57483589+tomer-w@users.noreply.github.com>
2025-08-21 16:50:17 +0300

    Add missing enum

--- victron_mqtt/__init__.py
+++ victron_mqtt/__init__.py
@@ -7,7 +7,7 @@ from .device import Device
 from .hub import Hub, CannotConnectError, ProgrammingError, NotConnectedError
 from .metric import Metric
 from .switch import Switch
-from ._victron_enums import DeviceType, InverterState, InverterMode, GenericOnOff, EvChargerMode, InverterOverloadAlarmEnum, FluidType, TemperatureType, TemperatureStatus, SolarChargerState, ESSMode, MultiState, DESSReactiveStrategy, DESSStrategy, DESSErrorCode, DESSRestrictions
+from ._victron_enums import DeviceType, InverterState, InverterMode, GenericOnOff, EvChargerMode, InverterOverloadAlarmEnum, FluidType, TemperatureType, TemperatureStatus, SolarChargerState, ESSMode, MultiState, DESSReactiveStrategy, DESSStrategy, DESSErrorCode, DESSRestrictions, VictronDeviceEnum
 
 __all__ = [
     "Hub",
@@ -38,4 +38,5 @@ __all__ = [
     "DESSStrategy",
     "DESSErrorCode",
     "DESSRestrictions",
+    "VictronDeviceEnum",
 ]
